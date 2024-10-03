7b1ac08c774e9bac6fa883838d99dce18c44a4ad
karlagarcia1 <2302087@utrivieramaya.edu.mx>
2024-10-03 08:36:17 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    const navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach((item, index) => {
+        setTimeout (() => {
+            item.classList.add('show');
+        }, 100 * index);
+ 
+    })
+   
+})  
